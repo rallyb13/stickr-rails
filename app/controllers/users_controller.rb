bc57759b01c329d94ed1ee2d99cf250b8f1a888c
crystal <crystalcontrerasgrossman@gmail.com>
2015-03-18 14:24:16 -0700

    reroute to profile page after registering user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,20 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome Aboard!"
-      redirect_to '/'
+      render :show
     else
       flash[:alert] = "There was a problem creating your account. Sucka."
       redirect_to :back
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
