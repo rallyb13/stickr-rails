39a0cdf49af853f7c846f8b97819404af461fdd0
Benjamin Herson <benjaminherson@gmail.com>
2015-03-18 11:30:44 -0700

    add authentication

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+gem 'pg'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'bcrypt-ruby', '~> 3.1.5', require: 'bcrypt'
+
+group :development do
+  gem 'byebug'
+  gem 'web-console'
+  gem 'spring'
+  gem 'quiet_assets'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'pry'
+  gem 'shoulda-matchers'
+  gem 'capybara'
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome Aboard!"
+      redirect_to '/'
+    else
+      flash[:alert] = "There was a problem creating your account. Sucka."
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
